d379a08d267b948131b2dd828f8730359a6f1ab2
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-06 02:04:40 -0500

    agregando lógica de loguearse de usuario en Login

--- frontend-citas/src/app/pages/login/login.ts
+++ frontend-citas/src/app/pages/login/login.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-  
+  username = '';
+  password = '';
+  mensaje = '';
+
+  constructor(private auth: Auth) {}
+
+  login() {
+    this.auth.login({ username: this.username, password: this.password }).subscribe({
+      next: (res: any) => {
+        this.auth.guardarToken(res.access);
+        localStorage.setItem('username', this.username);
+        this.mensaje = 'Login exitoso';
+        location.href = '/';  // redirigir al inicio
+      },
+      error: err => this.mensaje = 'Error de login'
+    });
+  }
 
 }
