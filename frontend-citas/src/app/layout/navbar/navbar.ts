34c0fc7769badd95d9393c0cbaba174fc86dcd0e
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-06 01:18:01 -0500

    mostrando la opción de login en el navbar

--- frontend-citas/src/app/layout/navbar/navbar.ts
+++ frontend-citas/src/app/layout/navbar/navbar.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  get usuarioLogueado(): boolean {
+    return localStorage.getItem('token') !== null;
+  }
+  get nombreUsuario(): string {
+    return localStorage.getItem('username') || '';
+  }
+  cerrarSesion() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    location.reload();
+  }
 
 }
