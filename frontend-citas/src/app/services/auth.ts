8b396c7722799450f15662e2ba30b8230de58c92
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-06 01:45:26 -0500

    creando un servicio para manejar los datos

---
+++ frontend-citas/src/app/services/auth.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+  private apiUrl = 'http://localhost:8000/api';
+
+    constructor(private http: HttpClient) {}
+
+    login(credentials: { username: string; password: string }) {
+      return this.http.post(`${this.apiUrl}/login/`, credentials);
+    }
+
+    register(data: { username: string; password: string; email: string }) {
+      return this.http.post(`${this.apiUrl}/register/`, data);
+    }
+
+    guardarToken(token: string) {
+      localStorage.setItem('token', token);
+    }
+
+    obtenerToken() {
+      return localStorage.getItem('token');
+    }
+
+    cerrarSesion() {
+      localStorage.removeItem('token');
+  }
+}
