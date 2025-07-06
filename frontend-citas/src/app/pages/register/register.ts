10a0eb17ce795e75782f39a960e112a21cd20dd6
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-06 01:54:11 -0500

    agregando lógica de registro de usuario en Register

--- frontend-citas/src/app/pages/register/register.ts
+++ frontend-citas/src/app/pages/register/register.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+  username = '';
+  password = '';
+  email = '';
+  mensaje = '';
+
+  constructor(private auth: Auth) {}
+
+  registrar() {
+    this.auth.register({ username: this.username, password: this.password, email: this.email })
+      .subscribe({
+        next: res => this.mensaje = '¡Usuario creado!',
+        error: err => this.mensaje = err.error.error || 'Error al registrar'
+      });
+      
+  }
 
 }
