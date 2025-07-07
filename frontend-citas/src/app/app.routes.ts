2615702b5f54782651881eb85be0ce2bc3d77dcb
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-06 23:14:54 -0500

    Añadiendo ruta

--- frontend-citas/src/app/app.routes.ts
+++ frontend-citas/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Nosotros } from './pages/nosotros/nosotros';
+import { ReservarCitaComponent } from './features/reservar-cita/reservar-cita';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'nosotros', component: Nosotros },
+    { path: 'reservar-cita', component: ReservarCitaComponent }
 ];
