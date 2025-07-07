4713d4cce86c5c94dfd5bbeb061e28bf347a3115
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-06 23:06:05 -0500

    Creacion del componente reservar-cita

---
+++ frontend-citas/src/app/features/reservar-cita/reservar-cita.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservar-cita',
+  imports: [],
+  templateUrl: './reservar-cita.html',
+  styleUrl: './reservar-cita.css'
+})
+export class ReservarCita {
+
+}
