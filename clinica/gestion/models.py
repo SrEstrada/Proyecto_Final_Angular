b34c823ec773b247f8c2282a7e40f857c4bbec33
SrEstrada <sestradaa@unsa.edu.pe>
2025-07-06 23:08:26 -0500

    Creando los modelos Especialidad y Médico, además un superusuario para pruebas

--- clinica/gestion/models.py
+++ clinica/gestion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Especialidad(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Medico(models.Model):
+    nombres = models.CharField(max_length=100)
+    correo = models.EmailField()
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombres} - {self.especialidad.nombre}"
