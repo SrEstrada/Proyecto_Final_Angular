89a894b95e7f9dfe0d2329f60458c3819990ad66
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-06 23:23:37 -0500

    Añadiendo las views en views.py

--- clinica/citas/views.py
+++ clinica/citas/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+def angular_app(request):
+    return render(request, 'index.html')
+
+@csrf_exempt
+@api_view(['POST'])
+def crear_cita(request):
+    try:
+        especialidad = request.data.get('especialidad')
+        fecha = request.data.get('fecha')
+        hora = request.data.get('hora')
+
+        print(f"Cita recibida: {especialidad}, {fecha}, {hora}")
+
+        return Response({"mensaje": "Cita creada con éxito"}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        print("Error al crear cita:", e)
+        return Response({"error": "Error al crear cita"}, status=status.HTTP_400_BAD_REQUEST)
+
