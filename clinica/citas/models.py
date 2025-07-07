b264aaa84629734472b2ad5f9e62633ccac4ac2d
Diego Hans Schreiber Landeo <dschreiber@unsa.edu.pe>
2025-07-06 22:53:57 -0500

    Añadiendo clase cita en models.py

--- clinica/citas/models.py
+++ clinica/citas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cita(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=100)
+    medico = models.CharField(max_length=100)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.medico} - {self.fecha} {self.hora}"
