7217cf3725d818670cefdce719c00fe9df81c1df
SrEstrada <sestradaa@unsa.edu.pe>
2025-07-06 23:51:29 -0500

    modelos Especialidad y Medico creados con API JSON habilitada usando Django REST Framework

--- clinica/gestion/views.py
+++ clinica/gestion/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Especialidad, Medico
+from .serializers import EspecialidadSerializer, MedicoSerializer
+
 
 # Create your views here.
 def angular_app(request):
     return render(request, 'index.html')
+
+@api_view(['GET'])
+def listar_especialidades(request):
+    especialidades = Especialidad.objects.all()
+    serializer = EspecialidadSerializer(especialidades, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def medicos_por_especialidad(request):
+    especialidad_id = request.GET.get('especialidad')
+    medicos = Medico.objects.filter(especialidad_id=especialidad_id)
+    serializer = MedicoSerializer(medicos, many=True)
+    return Response(serializer.data)
