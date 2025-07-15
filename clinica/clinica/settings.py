dfa2055d4900b6bbebfb5a3fcf569eff3f64328c
SrEstrada <sestradaa@unsa.edu.pe>
2025-07-15 10:09:12 -0500

    un pequeño arreglo: habilitado CORS para permitir conexión de Angular a la API Djang

--- clinica/clinica/settings.py
+++ clinica/clinica/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'gestion',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +126,9 @@ STATICFILES_DIRS = [
 
 TEMPLATES[0]['DIRS'] = [
     os.path.join(BASE_DIR, 'gestion/templates')
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
 ]
 
 # Default primary key field type
