2c50b9e8674bd211fe8fc4b821154a6b29c6c7bf
Renzo <rmamaniq@unsa.edu.pe>
2025-07-21 13:48:22 -0500

    solucionando un error con el register

--- frontend-citas/src/app/pages/login/login.ts
+++ frontend-citas/src/app/pages/login/login.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Auth } from '../../services/auth';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
