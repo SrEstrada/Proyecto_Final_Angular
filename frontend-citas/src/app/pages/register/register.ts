177d6d5fa5e26f03fbd613de81a3f5f2b9579bb2
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-07-28 01:49:56 -0500

    agregando la verificacion para registrarse

--- frontend-citas/src/app/pages/register/register.ts
+++ frontend-citas/src/app/pages/register/register.ts
@@ -12,20 +12,29 @@ import { Auth } from '../../services/auth';
 export class Register {
   username = '';
   password = '';
+  confirmPassword = '';
   email = '';
   mensaje = '';
 
   constructor(private auth: Auth) {}
 
   registrar() {
-    this.auth.register({ username: this.username, password: this.password, email: this.email })
-      .subscribe({
-        next: res => {
-          this.mensaje = '¡Usuario creado!';
-          setTimeout(() => location.href = '/login', 1000); // Redirige después de 1 segundo
-        },
-        error: err => this.mensaje = err.error.error || 'Error al registrar'
-      });
+    if (this.password !== this.confirmPassword) {
+      this.mensaje = 'Las contraseñas no coinciden';
+      return;
+    }
+
+    this.auth.register({
+      username: this.username,
+      password: this.password,
+      email: this.email
+    }).subscribe({
+      next: res => {
+        this.mensaje = '¡Usuario creado!';
+        setTimeout(() => location.href = '/login', 1000);
+      },
+      error: err => this.mensaje = err.error?.error || 'Error al registrar'
+    });
   }
 
 }
